9aded5c000f9665e225f8636cbcb8a9f97535be0
nike4192 <nikita1234563915@gmail.com>
2021-03-03 00:13:56 +0500

    Update PhysicalValue.h

--- task-21-02-15/PhysicalValue.h
+++ task-21-02-15/PhysicalValue.h
@@ -1,17 +1,41 @@
 #ifndef PHYSICALVALUE_H
 #define PHYSICALVALUE_H
 
-enum PHYSICAL_UNITS { N, Pa, m2 };
+#include <iostream>
+#include <map>
 
-template<typename T>
+const enum PHYS_UNITS{ N, Pa, m, J };
+
 class PhysicalValue
 {
 protected:
-	PHYSICAL_UNITS unit;
-	T value;
+
+	enum PHYS_UNITS unit;
+	double value = 0;
+	int exponent = 1;
+
+	PhysicalValue(enum PHYS_UNITS _unit, double _value) : unit(_unit), value(_value) {};
 
 public:
-	PhysicalValue(T _value);
+
+	enum PHYS_UNITS getUnit() const { return unit; }
+	double getValue() const { return value; }
+	int getExponent() const { return exponent; }
+
+	/* Arithmetic */
+
+	PhysicalValue& operator+=(const PhysicalValue& pv);
+	PhysicalValue& operator-=(const PhysicalValue& pv);
+	PhysicalValue& operator*=(const PhysicalValue& pv);
+	PhysicalValue& operator/=(const PhysicalValue& pv);
+
+	PhysicalValue operator+(const PhysicalValue& pv);
+	PhysicalValue operator-(const PhysicalValue& pv);
+	PhysicalValue operator*(const PhysicalValue& pv);
+	PhysicalValue operator/(const PhysicalValue& pv);
+
+	PhysicalValue(double _value) : value(_value) {};
+	void print() const;
 };
 
 #endif
