a438e48f0b5bd67ad7c81ed19fc393210192fa73
nike4192 <nikita1234563915@gmail.com>
2021-03-04 19:19:49 +0500

    Update Amperage.h

--- task-21-02-15/Amperage.h
+++ task-21-02-15/Amperage.h
@@ -1,13 +1,15 @@
 #ifndef AMPERAGE_H
 #define AMPERAGE_H
 
+/* 1.3 Класс величины электрического тока */
+
 class Amperage
 {
 public:
 
 	Amperage(double _value) : value(_value) {};
 
-	double getValue();
+	double getValue(); // не const потому что инкремирует accessCounter (учитывает количество чтений данных из экземпляра класса)
 	void setValue(double _value) { value = _value; };
 
 	int getAccessCounter() const { return accessCounter; };
