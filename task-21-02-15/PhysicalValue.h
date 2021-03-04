ad58191ef76987ab941f294bed719c996593f3cb
nike4192 <nikita1234563915@gmail.com>
2021-03-04 19:26:18 +0500

    Update PhysicalValue.h

--- task-21-02-15/PhysicalValue.h
+++ task-21-02-15/PhysicalValue.h
@@ -4,18 +4,12 @@
 #include <iostream>
 #include <map>
 
+/* 5 Общий интерфейс для классов физических величин */
+
 const enum PHYS_UNITS{ N, Pa, m, J };
 
 class PhysicalValue
 {
-protected:
-
-	enum PHYS_UNITS unit;
-	double value = 0;
-	int exponent = 1;
-
-	PhysicalValue(enum PHYS_UNITS _unit, double _value) : unit(_unit), value(_value) {};
-
 public:
 
 	enum PHYS_UNITS getUnit() const { return unit; }
@@ -24,18 +18,29 @@ public:
 
 	/* Arithmetic */
 
+	/* 5.6 Осуществления с ними операций сложения/вычитания/умножения/деления через базовый интерфейс без dynamic_cast */
 	PhysicalValue& operator+=(const PhysicalValue& pv);
 	PhysicalValue& operator-=(const PhysicalValue& pv);
 	PhysicalValue& operator*=(const PhysicalValue& pv);
 	PhysicalValue& operator/=(const PhysicalValue& pv);
 
-	PhysicalValue operator+(const PhysicalValue& pv);
-	PhysicalValue operator-(const PhysicalValue& pv);
-	PhysicalValue operator*(const PhysicalValue& pv);
-	PhysicalValue operator/(const PhysicalValue& pv);
+	PhysicalValue operator+(const PhysicalValue& pv) const;
+	PhysicalValue operator-(const PhysicalValue& pv) const;
+	PhysicalValue operator*(const PhysicalValue& pv) const;
+	PhysicalValue operator/(const PhysicalValue& pv) const;
 
 	PhysicalValue(double _value) : value(_value) {};
+
+	/* 5.4.	Вывод с общего интерфейса величин и их наименований. */
 	void print() const;
+
+protected:
+
+	enum PHYS_UNITS unit;
+	double value = 0;
+	int exponent = 1;
+
+	PhysicalValue(enum PHYS_UNITS _unit, double _value) : unit(_unit), value(_value) {};
 };
 
 #endif
